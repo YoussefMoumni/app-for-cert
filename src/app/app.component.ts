403ecc605b80d91fa82433b0fd3707a3f7304591
YoussefMoumni <youssef.moumni1013@gmail.com>
2024-07-15 10:34:26 +0100

    feat: init cocktails listing component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CocktailsListingComponent } from './cocktails-listing/cocktails-listing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterModule ],
+  imports: [ RouterModule,CocktailsListingComponent ],
   templateUrl: 'app.component.html',
 })
 export class AppComponent {
--- src/app/cocktails-listing/cocktails-listing.component.ts
+++ src/app/cocktails-listing/cocktails-listing.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CocktailsListingService } from '../services/cocktails-listing.service';
+import { Cocktail } from '../types/cocktail.types';
 
 @Component({
   selector: 'app-cocktails-listing',
@@ -8,5 +10,14 @@ import { Component } from '@angular/core';
   styleUrl: './cocktails-listing.component.scss'
 })
 export class CocktailsListingComponent {
+  cocktails: Cocktail[] = [];
 
+  constructor(private cocktailsListingService: CocktailsListingService) { }
+
+  ngOnInit(): void {
+    this.cocktailsListingService.getCocktails().subscribe({
+      next: (data) => this.cocktails = data,
+      error: (error) => console.error('Error!', error)
+    });
+  }
 }
